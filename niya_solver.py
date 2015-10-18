165b03f2c8024fc4147c5e71740f4ab6bb81c2a9
willij6 <willij6@uw.edu>
2015-10-17 20:54:57 -0700

    Scores are now pairs, and closures are thrown into the recursion

--- niya_solver.py
+++ niya_solver.py
@@ -8,57 +8,61 @@
 def code(i,j):
     return i*4 + j
 
-checks = []
+def get_checks():
+    for i in range(4):
+        yield([code(i,j) for j in range(4)])
+    for i in range(4):
+        yield([code(j,i) for j in range(4)])
+    yield([code(i,i) for i in range(4)])
+    yield([code(i,3-i) for i in range(4)])
+    for i in range(3):
+        for j in range(3):
+            t = []
+            for ii in range(2):
+                for jj in range(2):
+                    t.append(code(i+ii,j+jj))
+            yield(t)
 
-def add_condition(x):
-    checks.append(tuple(x))
+            
+checks = list(get_checks())
 
-for i in range(4):
-    add_condition([code(i,j) for j in range(4)])
-for i in range(4):
-    add_condition([code(j,i) for j in range(4)])
-add_condition([code(i,i) for i in range(4)])
-add_condition([code(i,3-i) for i in range(4)])
-for i in range(3):
-    for j in range(3):
-        t = []
-        for ii in range(2):
-            for jj in range(2):
-                t.append(code(i+ii,j+jj))
-        add_condition(t)
 
-# scores... 1000 for +1 win, -1000 for -1 win
-# For each possible attack, 1 point, 10 points, or 100 points for viability
-
-maxScore = 1000000
-
+# because python orders tuples lexicographically,
+# scores will now work this way:
+# (1,i) means that Plus wins by i points (i tiles remain)
+# (-1,-i) means that Minus wins by i points (i tiles remain)
+# (0,j) means neither player won (got the configuration of pieces)
+# in which case... we iterate over checks, and
+# for each position which could be completed, we assign 10**n pieces
+# for a row/column/etc with n friendly pieces and no enemy pieces
 
 def score(board):
     runningTotal = 0
     for c in checks:
-        plus = minus = 1
+        plus = 0
+        minus = 0
         for i in c:
             if(board[i][0] == -1):
                 if(board[i][1] == 1):
-                    plus *= 10
+                    plus += 1
                 else:
-                    minus *= 10
-        if(plus == 10000):
-            return maxScore*multiplier(board)
-        if(minus == 10000):
-            return -maxScore*multiplier(board)
-        if plus > 1 and minus > 1:
-            continue
-        runningTotal += plus
-        runningTotal -= minus
-    return runningTotal
+                    minus += 1
+        if plus==4:
+            return (1,tiles_left(board))
+        if minus==4:
+            return (-1,-tiles_left(board))
+        if plus > 0 and minus == 0:
+            runningTotal += 10**plus
+        elif minus > 0 and plus == 0:
+            runningTotal -= 10**plus
+    return (0,runningTotal)
 
-def multiplier(board):
-    mult = 1
+def tiles_left(board):
+    count = 0
     for i in range(16):
         if(board[i][0] != -1):
-            mult += 1
-    return mult
+            count += 1
+    return count
 
 
 def name_piece(t):
@@ -76,85 +80,111 @@ def display(board):
             s += name_piece(board[code(i,j)]) + " "
         print(s)
 
-def build_lookup(board):
+
+
+
+# FUN WITH CLOSURES
+
+def bestMoveAndScore(board,who,previous,lookahead):
     lookup = {}
+    for i in range(4):
+        for j in range(4):
+            lookup[(i,j)] = False
     for i in range(16):
         lookup[board[i]] = i
-    return lookup
 
-def next_moves(board,previous,lookup):
-    running_list = []
-    def helper(t):
-        if(t == previous):
-            return
-        if(board[lookup[t]] == t):
-            running_list.append(lookup[t])
-    for i in range(4):
-        t = (previous[0],i)
-        helper(t)
-    for i in range(4):
-        t = (i,previous[1])
-        helper(t)
-    return running_list
+    def next_moves():
+        running_list = []
+        def helper(t):
+            if(t == previous):
+                return
+            if(lookup[t] and board[lookup[t]] == t):
+                running_list.append(lookup[t])
+        for i in range(4):
+            helper((previous[0],i))
+        for i in range(4):
+            helper((i,previous[1]))
+        return running_list
 
+    # returns (move,score), where
+    # min(ub,max(lb,score)) = min(ub,max(lb,true_score))
+    # and where move is a move attaining score
+    # assuming ub < score < lb and the game ain't over
+    # and lookahead is positive...
+    # else move is garbage/None
+    def recursive_eval(ub,lb):
+        nonlocal who, previous, lookahead
+        # nonlocal board, lookup... except they're not getting assigned to
 
+        # print("Considering this position with lookahead = %d:" % lookahead)
+        # display(board)
+        
+        moves = next_moves()
+        if(len(moves) == 0):
+            return (None,(-who,-who*tiles_left(board)))
+        s = score(board)
+        if(lookahead <= 0 or s[0] != 0): # s[0] != 0 checks for game over
+            return (None,s)
 
-    # return -1 to indicate no move available
-    # if lookahead = 0, the returned move is garbage
-    # if the game is OVER, the returned move is garbage
-def bestMoveAndScore(board,who,previous,lookup,lookahead,ub,lb):
-    moves = next_moves(board,previous,lookup)
-    if(len(moves) == 0):
-        return (-1,-who*maxScore*multiplier(board))
-    if(lookahead <= 0):
-        return (-1,score(board))
-    ess = score(board)
-    if(ess >= maxScore or ess <= -maxScore):
-        return (-1,ess)
-    pairs = []
-    for m in moves:
-        t = board[m]
-        board[m] = (-1,who)
-        s = score(board)*who
-        pairs.append((m,s))
-        board[m] = t
-    pairs.sort(key = lambda x : x[1], reverse = True)
-    moves = [x[0] for x in pairs]
-    # print("Sorted move list is " + str(moves))
-
-    # ok now handle the moves
-    if(who > 0):
-        bestScore = lb
-        bestMove = -1
+        # sort the moves
+        pairs = []
         for m in moves:
             t = board[m]
             board[m] = (-1,who)
-            (junk,s) = bestMoveAndScore(board,-who,t,lookup,lookahead - 1,ub,bestScore)
+            s = score(board)*who
+            pairs.append((m,s))
             board[m] = t
-            if (s > bestScore):
-                bestMove = m
-                bestScore = s
-            if bestScore >= ub:
-                return (bestMove,bestScore)
-        return (bestMove,bestScore)
-    else:
-        bestScore = ub
-        bestMove = -1
-        for m in moves:
-            t = board[m]
-            board[m] = (-1,who)
-            (junk,s) = bestMoveAndScore(board,-who,t,lookup,lookahead - 1,bestScore,lb)
-            board[m] = t
-            if (s < bestScore):
-                bestMove = m
-                bestScore = s
-            if bestScore <= lb:
-                return (bestMove,bestScore)
-        return (bestMove,bestScore)
+        pairs.sort(key = lambda x : x[1], reverse=True)
+        moves = [x[0] for x in pairs]
+        # now the moves are sorted
 
+        if(who > 0):
+            bestScore = lb
+            bestMove = None
 
+            lookahead -= 1
+            who = -1
+            old_prev = previous
+            for m in moves:
+                t = board[m]
+                previous = t # paranoia
+                board[m] = (-1,1)
+                (junk,s) = recursive_eval(ub,bestScore)
+                board[m] = t
+                if s > bestScore:
+                    bestMove = m
+                    bestScore = s
+                if bestScore >= ub:
+                    break
+            previous = old_prev
+            who = 1
+            lookahead += 1
+            return (bestMove,bestScore)
+        else:
+            bestScore = ub
+            bestMove = None
 
-       
+            lookahead -= 1
+            who = 1
+            old_prev = previous
+            for m in moves:
+                t = board[m]
+                previous = t
+                board[m] = (-1,-1)
+                (junk,s) = recursive_eval(bestScore,lb)
+                board[m] = t
+                if s < bestScore:
+                    bestMove = m
+                    bestScore = s
+                if bestScore <= lb:
+                    break
+            previous = old_prev
+            who = -1
+            lookahead += 1
+            return (bestMove,bestScore)
+        
+    return recursive_eval((2,0),(-2,0))
+
 
        
     
@@ -165,8 +195,8 @@ board = [(i % 4,i//4) for i in range(16)]
 shuffle(board)
 #print(board)
 display(board)
-ell = build_lookup(board)
-#print(ell)
+# ell = build_lookup(board)
+# #print(ell)
 curr = randrange(15)
 while(curr in [5,6,9,10]):
     curr = randrange(15)
@@ -177,12 +207,10 @@ display(board)
 who = -1
 over = False
 while not over:    
-    mov = next_moves(board,prev,ell)
     ahead = 15
     # print("Analyzing with lookahead " + str(ahead))
-    (mov,scor) = bestMoveAndScore(board,who,prev,ell,
-                                  ahead,100*maxScore,-100*maxScore)
-    if mov == -1:
+    (mov,scor) = bestMoveAndScore(board,who,prev,ahead)
+    if mov is None:
         over = True
         print("Player " + str(who) + " forfeits")
     else:
@@ -194,15 +222,10 @@ while not over:
         # print("Score is now " + str(score(x)))
         who = -who
         ess = score(board)
-        if (ess >= maxScore):
+        if (ess[0] == 1):
             over = True
             print("Player 1 wins!")
-        elif (ess <= -maxScore):
+        elif (ess[0] == -1):
             over = True
             print("Player -1 wins!")
 
-
-
-
-# class GameState:
-#     def __init__(self, 
